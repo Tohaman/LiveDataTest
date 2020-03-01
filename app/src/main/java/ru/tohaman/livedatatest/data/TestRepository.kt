a5758413ce12299d8efa196982e58595d540f9f6
TohaRG2 <avrozovu@yandex.ru>
2020-03-01 21:51:20 +0300

    Добавляем Репозиторий

--- app/src/main/java/ru/tohaman/livedatatest/data/TestRepository.kt
+++ app/src/main/java/ru/tohaman/livedatatest/data/TestRepository.kt
@@ -1,15 +1,26 @@
 package ru.tohaman.livedatatest.data
 
-import ru.tohaman.livedatatest.ioThread
+import kotlinx.coroutines.delay
+
+/**
+ * Для этого примера казалось бы лишний класс, который в реальных условиях позволяет
+ * реализовать, например кэширование данных. Т.е. программа обращается за данными в
+ * репозиторий, а уже репозиторий решает откуда их брать (из локальной базы, интернета
+ * или откуда-то еще)
+ */
+
 
 class TestRepository {
     private val dao = testDatabase.testDao
 
     suspend fun getAllItems(): List<TestItem> {
+//        delay(5000)
         return dao.getAllItems()
     }
 
     suspend fun getByNum(num : Int) : List<TestItem> {
+        //Имитирунм задержку получения данных
+        delay(2000) //2 сек
         return dao.getByNum(num)
     }
 
